740082782a7364302a72465cbc42c730c27ab092
cristim806 <cristian.merticaru8806@yahoo.com>
2021-01-24 12:44:05 +0200

    First commit

---
+++ src/main/java/com/cristianov/codebetter/CodebetterApplication.java
@@ -0,0 +1,13 @@
+package com.cristianov.codebetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebetterApplication.class, args);
+	}
+
+}
