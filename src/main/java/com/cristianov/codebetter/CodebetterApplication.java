c047b2bb319713c3e71caa7f89369d3bfe3a531f
cristim806 <cristian.merticaru8806@yahoo.com>
2021-01-25 01:46:09 +0200

    Commit frontend - adding form to modal

--- src/main/java/com/cristianov/codebetter/CodebetterApplication.java
+++ src/main/java/com/cristianov/codebetter/CodebetterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodebetterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodebetterApplication.class, args);
+		 SpringApplication.run(CodebetterApplication.class, args);
 	}
 
 }
