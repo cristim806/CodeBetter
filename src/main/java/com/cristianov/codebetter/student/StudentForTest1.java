bd40c213c235d9a64bea8eac0214f2bf2004f839
cristim806 <cristian.merticaru8806@yahoo.com>
2021-02-28 10:44:22 +0200

    Change 1 feature4

--- src/main/java/com/cristianov/codebetter/student/StudentForTest1.java
+++ src/main/java/com/cristianov/codebetter/student/StudentForTest1.java
@@ -1,4 +1,25 @@
 package com.cristianov.codebetter.student;
 
 public class StudentForTest1 {
+
+    private String fieldOneFeature4;
+    private String fieldTwoFeature4;
+
+    public String getFieldOneFeature4() {
+        return fieldOneFeature4;
+    }
+
+    public void setFieldOneFeature4(String fieldOneFeature4) {
+        this.fieldOneFeature4 = fieldOneFeature4;
+    }
+
+    public String getFieldTwoFeature4() {
+        return fieldTwoFeature4;
+    }
+
+    public void setFieldTwoFeature4(String fieldTwoFeature4) {
+        this.fieldTwoFeature4 = fieldTwoFeature4;
+    }
+
+
 }
